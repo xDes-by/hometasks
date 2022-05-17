6b739c7d1a91f91275f10c7b3782d140d3bc8e42
Viktor Mekka <italyevil@mail.ru>
2022-05-17 09:35:19 +0300

    hometask_1

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "Viktor"
+favorite_color = "dark-blue"
+favorite_music_group = "AnnenMayKantereit"
+favorite_film = "The Shawshank Redemption"
+print("Hello, my name is", name+".", '\n' "My favorite color", favorite_color+",",  "favorite music group", favorite_music_group, "and favorite film -", favorite_film+".")
+
