a8882a263588fda0d089ebc9512deda89f109a18
Viktor Mekka <italyevil@mail.ru>
2022-05-18 20:27:40 +0300

    test

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ favorite_color = "dark-blue"
 favorite_music_group = "AnnenMayKantereit"
 favorite_film = "The Shawshank Redemption"
 print("Hello, my name is", name+".", '\n' "My favorite color", favorite_color+",",  "favorite music group", favorite_music_group, "and favorite film -", favorite_film+".")
-
+print("test")
