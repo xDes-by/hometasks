b3f71e97e3fc07c491f57ccdcb5a49e876bf90b4
Viktor Mekka <italyevil@mail.ru>
2022-05-24 15:33:13 +0300

    fix_fibonachi

--- hometask_3.py
+++ hometask_3.py
@@ -5,49 +5,41 @@
 черепаха и дополнительные задания в другий файлах
 """
 
-# number_1 = int(input("Введите число: "))
-# operator = input("Введите действие: ")
-# number_2 = int(input("Введите число: "))
-#
-# result = ''
-# if operator == "*":
-#     result = number_1 * number_2
-# elif operator == "/":
-#     result = number_1 / number_2
-# elif operator == "-":
-#     result = number_1 - number_2
-# elif operator == "+":
-#     result = number_1 + number_2
-# else:
-#     print("а больше я ничего не умею=)")
-#
-# print(result)
+number_1 = int(input("Введите число: "))
+operator = input("Введите действие: ")
+number_2 = int(input("Введите число: "))
 
-# ======================= 2 ВАРИАНТ ======================
-# result = ''
-# while True:
-#     task = input("Введите уравнение c '+','-','*' или '/': ")
-#     if "*" in task:
-#         number_list = task.split("*")
-#         result = int(number_list[0]) * int(number_list[1])
-#     elif "-" in task:
-#         number_list = task.split("-")
-#         result = int(number_list[0]) - int(number_list[1])
-#     elif "+" in task:
-#         number_list = task.split("+")
-#         result = int(number_list[0]) + int(number_list[1])
-#     elif "/" in task:
-#         number_list = task.split("/")
-#         result = int(number_list[0]) / int(number_list[1])
-#     else:
-#         print("Мне такое не подходит, ухожу в тень...")
-#         break
-#     print(result)
+result = ''
+if operator == "*":
+    result = number_1 * number_2
+elif operator == "/":
+    result = number_1 / number_2
+elif operator == "-":
+    result = number_1 - number_2
+elif operator == "+":
+    result = number_1 + number_2
+else:
+    print("а больше я ничего не умею=)")
+
+print(result)
 
 # ======================= 2 ВАРИАНТ ======================
 result = ''
 while True:
     task = input("Введите уравнение c '+','-','*' или '/': ")
-    if "*" or "+" in task:
-        print("p")
-
+    if "*" in task:
+        number_list = task.split("*")
+        result = int(number_list[0]) * int(number_list[1])
+    elif "-" in task:
+        number_list = task.split("-")
+        result = int(number_list[0]) - int(number_list[1])
+    elif "+" in task:
+        number_list = task.split("+")
+        result = int(number_list[0]) + int(number_list[1])
+    elif "/" in task:
+        number_list = task.split("/")
+        result = int(number_list[0]) / int(number_list[1])
+    else:
+        print("Мне такое не подходит, ухожу в тень...")
+        break
+    print(result)
